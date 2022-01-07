090552fa69f876fac9f3c3f0012663f684673a5d
Sasun Steanyan <sasunstepanyan777@gmail.com>
2022-01-08 01:08:16 +0400

    created preloadingSubject

--- projects/ngs-hover-preload/src/lib/ngs-hover-preload.service.ts
+++ projects/ngs-hover-preload/src/lib/ngs-hover-preload.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NgsHoverPreloadService {
-  constructor() {}
+  preloadingSubject$ = new Subject<string>();
+
+  preload(path: string) {
+    this.preloadingSubject$.next(path);
+  }
 }
