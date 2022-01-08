e581093ba84995c83ffe4b68ffed5b3c695bdc8c
Sasun Steanyan <sasunstepanyan777@gmail.com>
2022-01-08 22:39:43 +0400

    move hover preload service into hover preload module

--- projects/ngs-hover-preload/src/lib/ngs-hover-preload.module.ts
+++ projects/ngs-hover-preload/src/lib/ngs-hover-preload.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { NgsHoverPreloadService } from './ngs-hover-preload.service';
 import { NgsPreloadOnHoverDirective } from './ngs-preload-on-hover.directive';
 
 @NgModule({
   declarations: [NgsPreloadOnHoverDirective],
+  providers: [NgsHoverPreloadService],
   imports: [],
   exports: [NgsPreloadOnHoverDirective],
 })
